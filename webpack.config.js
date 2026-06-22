a0ca8f79c6c6494748023070a8ca6be956024b13
禾明 <543369008@qq.com>
2026-06-22 11:43:06 +0800

    打包产物单独抽离CSS文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: 'development',
+  mode: isProd ? 'production' : 'development',
   entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      // template: './src/index.html'
-      template: path.resolve(__dirname, 'public/index.html'),
-    })
-  ],
-   module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    isProd &&
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash].css',
+      }),
+  ].filter(Boolean),
 };
