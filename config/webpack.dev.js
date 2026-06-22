a01107ea363443d4f316b844e7eba70896f4fdc3
禾明 <543369008@qq.com>
2026-06-22 11:52:27 +0800

    工程化打包环境分离

---
+++ config/webpack.dev.js
@@ -0,0 +1,12 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    hot: true,
+    open: true,
+    port: 3000,
+  },
+});
---
+++ config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
+    }),
+  ],
+});
