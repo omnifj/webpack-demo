006ec1b72ced121f93297acef8ce199e75880395
禾明 <543369008@qq.com>
2026-06-22 11:25:40 +0800

    打包HTML并自动注入 bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      // template: './src/index.html'
+      template: path.resolve(__dirname, 'public/index.html'),
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
